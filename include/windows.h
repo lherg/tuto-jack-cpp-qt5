26b8366ba7281bc26be6140b7cb7a37594167702
lherg <lherg@home.fr>
2023-02-18 18:36:08 +0100

    Ajout licence

--- include/windows.h
+++ include/windows.h
@@ -1,3 +1,21 @@
+/* window.h
+ *
+ * Copyright (C) 2023 https://github.com/lherg
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef WIDGET_H
 #define WIDGET_H
 
@@ -22,7 +40,7 @@ class Widget : public QWidget
 
         std::atomic<float> gain;
         std::mutex enabled_mutex;
-        
+
         Widget(QWidget *parent = nullptr);
         ~Widget();
 
